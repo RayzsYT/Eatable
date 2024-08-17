27bdd12c79c275a54d0eb010d396efa88f35666f
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-08-17 02:22:22 +0200

    Add

---
+++ src/main/java/de/rayzs/eatable/utils/MessageUtil.java
@@ -0,0 +1,67 @@
+package de.rayzs.eatable.utils;
+
+import de.rayzs.eatable.utils.configuration.*;
+import org.bukkit.command.CommandSender;
+import org.bukkit.ChatColor;
+import java.util.*;
+
+public class MessageUtil {
+
+    private static final HashMap<String, Message> MESSAGES = new HashMap<>();
+    private static final Message ERROR_MESSAGE = new Message("&c");
+    private static final ConfigurationBuilder FILE = Configurator.get("messages");
+    
+    public static void clear() {
+        MESSAGES.clear();
+    }
+    
+    public static void send(CommandSender sender, String key) {
+        Message message = null;
+        if(MESSAGES.containsKey(key)) {
+            try {
+
+                Object obj = FILE.get(key);
+
+                if(obj instanceof String line)
+                    message = new Message(line);
+                else if(obj instanceof List<?> lines)
+                    message = new Message((List<String>) lines);
+
+                if(message != null) MESSAGES.put(key, message);
+
+            } catch (Exception exception) {
+                Logger.warning("Failed to load message from message.yml: " + key);
+            }
+        }
+
+        (message == null ? ERROR_MESSAGE : message).sendMessage(sender);
+    }
+    
+    private static class Message {
+        
+        private final List<String> LINES = new ArrayList<>();
+
+        public Message(List<String> lines) {
+            LINES.addAll(lines);
+        }
+
+        public Message(String... lines) {
+            LINES.addAll(List.of(lines));
+        }
+
+        public Message add(List<String> lines) {
+            LINES.addAll(lines);
+            return this;
+        }
+
+        public Message add(String... lines) {
+            LINES.addAll(List.of(lines));
+            return this;
+        }
+
+        public void sendMessage(CommandSender sender) {
+            for (String line : StringUtils.replaceList(LINES))
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', line));
+        }
+    }
+}
